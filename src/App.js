8de5a8d17854a4a3f53d7dc9a955e6816d3224b2
Truonghq97 <truonghq97@gmail.com>
2020-10-11 17:23:00 +0700

    add component header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
+
+import Header from "./components/header/Header";
 
 function App() {
   return (
     <div className="App">
-      <h1>Welcome to Facebook clone</h1>
+      {/* Header */}
+      <Header />
+
+      {/* App body */}
+      {/* Sidebar */}
+      {/* Feed */}
+      {/* Widgets */}
     </div>
   );
 }
---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBumJ8Stu9_yi4WZ1slaAvXL-u27BSydak",
+    authDomain: "facebook-clone-5166e.firebaseapp.com",
+    databaseURL: "https://facebook-clone-5166e.firebaseio.com",
+    projectId: "facebook-clone-5166e",
+    storageBucket: "facebook-clone-5166e.appspot.com",
+    messagingSenderId: "587684419604",
+    appId: "1:587684419604:web:507470dc234c8598006f01",
+    measurementId: "G-TWWC0N0B90"
+  };
